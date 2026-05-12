56e00104afc6ca52db633eceb0bc5912c28501b7
Rendy Saputra <1814138+rensaputra@users.noreply.github.com>
2026-05-12 20:48:50 +1000

    chore: tidy up

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -138,7 +138,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Rendy Saputra. Built with Docusaurus.`,
     },
     prism: {
       theme: prismThemes.github,
