485cb650758a54b8169fd68168b58da3d72039fb
Rendy Saputra <1814138+rensaputra@users.noreply.github.com>
2026-07-12 00:07:53 +1000

    chore: revert config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -27,7 +27,7 @@ const config: Config = {
   organizationName: "rensaputra", // Usually your GitHub org/user name.
   projectName: "aws-docs", // Usually your repo name.
 
-  onBrokenLinks: "warn",
+  onBrokenLinks: "throw",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
